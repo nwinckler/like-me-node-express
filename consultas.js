74a85f5cdd65d983a03bf268b844e5a0361c25f5
Nicolas Winckler <nwinckler@hotmail.com>
2024-12-15 12:46:39 -0300

    Se suben requerimientos desafio Like Me Parte 2

--- consultas.js
+++ consultas.js
@@ -1,4 +1,4 @@
-// 2. Usar el paquete pg para conectarse e interactuar con la base de datos.
+// Like Me (Parte I). 2. Usar el paquete pg para conectarse e interactuar con la base de datos.
 
 const {Pool} = require('pg')
 const pool = new Pool({
@@ -10,16 +10,49 @@ const pool = new Pool({
 })
 
 const agregarPost = async(titulo, url, descripcion) =>{
-    const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3)"
-    const values = [titulo, url, descripcion]
-    const result = await pool.query(consulta, values)
-    console.log("Post agregado")
+    try {
+        const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3)"
+        const values = [titulo, url, descripcion]
+        const result = await pool.query(consulta, values)
+        console.log("Post agregado")  
+    } catch (error) {
+        throw new Error("Problemas al agregar un nuevo post")
+    }
 }
 
 const getPosts = async() =>{
-    const {rows} = await pool.query("SELECT * FROM posts")
-    console.log(rows)
-    return rows
+    try {
+        const {rows} = await pool.query("SELECT * FROM posts")
+        console.log(rows)
+        return rows   
+    } catch (error) {
+        throw new Error("Problemas al obtener los posts")
+    }
 }
 
-module.exports = {agregarPost, getPosts}
+const like = async(id) =>{
+    try {
+        const consulta = 
+        `UPDATE posts 
+        SET likes = 
+            CASE
+            WHEN likes is null THEN 1 ELSE likes + 1 END
+        WHERE id = $1`
+        const values = [id]
+        const result = await pool.query(consulta, values)
+    } catch (error) {
+        throw new Error("Problemas al agregar un like")
+    }
+}
+
+const eliminarPost = async(id) =>{
+    try {
+        const consulta = "DELETE FROM posts WHERE id = $1"
+        const values = [id]
+        const result = await pool.query(consulta, values)
+    } catch (error) {
+        throw new Error("Problemas al eliminar un post")
+    }
+}
+
+module.exports = {agregarPost, getPosts, like, eliminarPost}
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const { agregarPost, getPosts } = require('./consultas')
+const { agregarPost, getPosts, like, eliminarPost } = require('./consultas')
 const app = express()
 
-// 1. Habilitar los cors en el servidor utilizando el paquete de npm
+// Like Me (Parte I). 1. Habilitar los cors en el servidor utilizando el paquete de npm
 const cors = require('cors')
 
 app.use(express.json());
@@ -12,17 +12,54 @@ app.listen(3000, console.log('Servidor encendido'))
 app.use(cors())
 
 
-// 3. Crear una ruta GET con Express para devolver los registros de una tabla alojada en PostgreSQL.
+// Like Me (Parte I). 3. Crear una ruta GET con Express para devolver los registros de una tabla alojada en PostgreSQL.
 
 app.get("/posts", async (req,res) =>{
-    const posts = await getPosts()
-    res.json(posts)
+    try {
+        const posts = await getPosts()
+        res.json(posts)        
+    } catch (error) {
+        res.status(400).send(`Error: ${error.message}`)
+    }
 })
 
-// 4. Crear una ruta POST con Express para devolver los registros de una tabla alojada en PostgreSQL.
+// Like Me (Parte I). 4. Crear una ruta POST con Express para devolver los registros de una tabla alojada en PostgreSQL.
 
 app.post("/posts", async (req, res) =>{
-    const { titulo, url, descripcion } = req.body
-    await agregarPost(titulo, url, descripcion)
-    res.send("Post agregado con éxito")
+    try {
+        const { titulo, url, descripcion } = req.body
+        await agregarPost(titulo, url, descripcion)
+        res.send("Post agregado con éxito")            
+    } catch (error) {
+        res.status(400).send(`Error: ${error.message}`)
+    }
+    
 })
+
+// Like Me (Parte II). 1. Agregar una ruta PUT en una API REST y utilizarla para modificar registros en una tabla alojada en PostgreSQL
+
+app.put("/posts/like/:id", async(req,res)=>{
+    try {
+        const {id} = req.params
+        await like(id)
+        res.send("Like agregado con éxito")
+    } catch (error) {
+        res.status(400).send(`Error: ${error.message}`)
+    }
+})
+
+// Like Me (Parte II). 2. Agregar una ruta DELETE en una API REST y utilizarla para modificar registros en una tabla alojada en PostgreSQL
+
+app.delete("/posts/:id", async(req,res) =>{
+    try {
+        const {id} = req.params
+        await eliminarPost(id)
+        res.send("Post eliminado con éxito")
+
+    } catch (error) {
+        res.status(400).send(`Error: ${error.message}`)
+    }
+})
+
+// Like Me (Parte II). 3. Capturar los posibles errores en una consulta SQL realizada con el paquete pg usando la sentencia try catch
+// Se agrega la sentencia try/catch a todo
